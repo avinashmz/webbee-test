7aa236670ac9dfe4e512a05c805f7f37a2af8fb8
Avinash Zala <info@xpertdeveloper.com>
2021-09-02 20:11:21 +0530

    Removed Slot Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,12 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        $this->call(SlotSeeder::class);
-
         $this->call(EventSeeder::class);
 
         $this->call(CustomerSeeder::class);
-
     }
 }
