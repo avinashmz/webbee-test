b81f7898330d3cb7f10a559ae60ffec2d598239c
Avinash Zala <info@xpertdeveloper.com>
2021-09-02 19:32:11 +0530

    Customer and Event Seeder Added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,10 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call(SlotSeeder::class);
+
+        $this->call(EventSeeder::class);
+
+        $this->call(CustomerSeeder::class);
+
     }
 }
