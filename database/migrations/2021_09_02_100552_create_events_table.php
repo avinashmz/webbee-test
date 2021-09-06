ff443aa871a9808f4ef220183079635780c58f96
Avinash Zala <info@xpertdeveloper.com>
2021-09-06 19:37:28 +0530

    New fields added as discussed

--- database/migrations/2021_09_02_100552_create_events_table.php
+++ database/migrations/2021_09_02_100552_create_events_table.php
@@ -23,6 +23,10 @@ class CreateEventsTable extends Migration
             $table->tinyInteger('slot_duration')->default(30);
             $table->tinyInteger('future_day_max')->default(5)->comment('Define number of days upto which user can book for a event in future.');
             $table->tinyInteger('minimum_time_gap')->default('10')->comment('Minimum time gap required in minutes to book an event.');
+            $table->string('day_start')->comment('Day Start for an Event Day')->default('08:00')->nullable();
+            $table->string('day_end')->comment('Day End for an Event Day')->default('20:00')->nullable();
+            $table->string('break_start')->comment('Break Start for an Event Day')->default('12:00')->nullable();
+            $table->string('break_end')->comment('Break End for an Event Day')->default('13:00')->nullable();
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
         });
